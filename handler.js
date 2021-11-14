f4b620d128ff7e3fa3281c359e93c1676fedd609
rahmad13 <93415263+rahmad13@users.noreply.github.com>
2021-11-14 12:23:40 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -28,43 +28,147 @@ module.exports = {
         let user = global.db.data.users[m.sender]
         if (typeof user !== 'object') global.db.data.users[m.sender] = {}
         if (user) {
-          if (!isNumber(user.exp)) user.exp = 0
-          if (!isNumber(user.limit)) user.limit = 10
-          if (!isNumber(user.lastclaim)) user.lastclaim = 0
-          if (!('registered' in user)) user.registered = false
-          if (!user.registered) {
-            if (!('name' in user)) user.name = this.getName(m.sender)
-            if (!isNumber(user.age)) user.age = -1
-            if (!isNumber(user.regTime)) user.regTime = -1
-          }
-          if (!isNumber(user.afk)) user.afk = -1
-          if (!('afkReason' in user)) user.afkReason = ''
-          if (!('banned' in user)) user.banned = false
-          if (!isNumber(user.level)) user.level = 0
-          if (!isNumber(user.call)) user.call = 0
-          if (!user.role) user.role = 'Bronze'
-          if (!('autolevelup' in user)) user.autolevelup = false
-          if (!isNumber(user.pc)) user.pc = 0
-          if (!isNumber(user.warning)) user.warning = 0
+            if (!isNumber(user.healt)) user.healt = 0
+            if (!isNumber(user.level)) user.level = 0
+            if (!isNumber(user.exp)) user.exp = 0
+            if (!isNumber(user.limit)) user.limit = 10
+            if (!isNumber(user.lastclaim)) user.lastclaim = 0
+            if (!isNumber(user.money)) user.money = 0
+            
+            if (!isNumber(user.diamond)) user.diamond = 0
+            if (!isNumber(user.iron)) user.iron = 0
+
+            if (!isNumber(user.common)) user.common = 0
+            if (!isNumber(user.uncommon)) user.uncommon = 0
+            if (!isNumber(user.mythic)) user.mythic = 0
+            if (!isNumber(user.legendary)) user.legendary = 0
+            if (!isNumber(user.pet)) user.pet = 0
+        
+            if (!isNumber(user.potion)) user.potion = 0
+            if (!isNumber(user.sampah)) user.sampah = 0
+            if (!isNumber(user.armor)) user.armor = 0
+            
+            if (!isNumber(user.kucing)) user.kucing = 0
+            if (!isNumber(user.kucinglastclaim)) user.kucinglastclaim = 0
+            if (!isNumber(user.kuda)) user.kuda = 0
+            if (!isNumber(user.kudalastclaim)) user.kudalastclaim = 0
+            if (!isNumber(user.rubah)) user.rubah = 0
+            if (!isNumber(user.rubahlastclaim)) user.rubahlastclaim = 0
+            if (!isNumber(user.anjing)) user.anjing = 0
+            if (!isNumber(user.anjinglastclaim)) user.anjinglastclaim = 0
+
+            if (!'banned' in user) user.banned = false
+            if (!'bannedReason' in user) user.bannedReason = ''
+            if (!isNumber(user.warn)) user.warn = 0
+
+            if (!isNumber(user.afk)) user.afk = -1
+            if (!'afkReason' in user) user.afkReason = ''
+        
+            if (!isNumber(user.anakkucing)) user.anakkucing = 0
+            if (!isNumber(user.anakkuda)) user.anakkuda = 0
+            if (!isNumber(user.anakrubah)) user.anakrubah = 0
+            if (!isNumber(user.anakanjing)) user.anakanjing = 0
+            if (!isNumber(user.makananpet)) user.makananpet = 0
+
+            if (!isNumber(user.antispam)) user.antispam = 0
+            if (!isNumber(user.antispamlastclaim)) user.antispamlastclaim = 0
+
+            if (!isNumber(user.kayu)) user.kayu = 0
+            if (!isNumber(user.batu)) user.batu = 0
+            if (!isNumber(user.emas)) user.emas = 0
+            if (!isNumber(user.string)) user.string = 0
+            if (!isNumber(user.sword)) user.sword = 0
+            if (!isNumber(user.sworddurability)) user.sworddurability = 0
+            if (!isNumber(user.pickaxe)) user.pickaxe = 0
+            if (!isNumber(user.pickaxedurability)) user.pickaxedurability = 0
+            if (!isNumber(user.pancing)) user.pancing = 0
+            if (!isNumber(user.fishingrod)) user.fishingrod = 0
+            if (!isNumber(user.fishingroddurability)) user.fishingroddurability = 0
+
+            if (!isNumber(user.lastadventure)) user.lastadventure = 0
+            if (!isNumber(user.lastfishing)) user.lastfishing = 0
+            if (!isNumber(user.lastberburu)) user.lastberburu = 0
+           
+            if (!isNumber(user.lastdungeon)) user.lastdungeon = 0
+            if (!isNumber(user.lastslot)) user.lastslot = 0
+            if (!isNumber(user.lastduel)) user.lastduel = 0
+            if (!isNumber(user.lastmining)) user.lastmining = 0
+            if (!isNumber(user.lasthunt)) user.lasthunt = 0
+            if (!isNumber(user.lastweekly)) user.lastweekly = 0
+            if (!isNumber(user.lastmonthly)) user.lastmontly = 0
+            if (!('registered' in user)) user.registered = false
+            if (!user.registered) {
+                if (!('name' in user)) user.name = this.getName(m.sender)
+                if (!isNumber(user.regTime)) user.regTime = -1
+            }
+            if (!('autolevelup' in user)) user.autolevelup = false
+            if (!('ah' in user)) user.ah = []
+            if (!('mission' in user)) user.mission = {}
           if (!('pasangan' in user)) user.pasangan = ''
         } else global.db.data.users[m.sender] = {
-          exp: 0,
-          limit: 10,
-          lastclaim: 0,
-          registered: false,
-          name: this.getName(m.sender),
-          age: -1,
-          regTime: -1,
-          afk: -1,
-          afkReason: '',
-          banned: false,
-          level: 0,
-          call: 0,
-          role: 'Bronze',
-          autolevelup: false,
-          pc: 0,
-          warning: 0,
-          pasangan: '',
+         healt: 100,
+            level: 0,
+            exp: 0,
+            limit: 10,
+            lastclaim: 0,
+            money: 0,
+            pasangan: '',
+            diamond: 0,
+            iron: 0,
+            common: 0,
+            uncommon: 0,
+            mythic: 0,
+            legendary: 0,
+            pet: 0,
+            potion: 0,
+            sampah: 0,
+            armor: 0,
+            kucing: 0,
+            kucinglastclaim: 0,
+            kuda: 0,
+            kudalastclaim: 0,
+            rubah: 0,
+            rubahlastclaim: 0,
+            anjing: 0,
+            anjinglastclaim: 0,
+            Banneduser: false,
+            BannedReason: '',
+            warn: 0,
+            afk: -1,
+            afkReason: '',
+            anakkucing: 0,
+            anakkuda: 0,
+            anakrubah: 0,
+            anakanjing: 0,
+            makananpet: 0,
+            antispam: 0,
+            antispamlastclaim: 0,
+            kayu: 0,
+            batu: 0,
+            string: 0,
+            sword: 0,
+            sworddurability: 0,
+            pickaxe: 0,
+            pickaxedurability: 0,
+            pancing: 0,
+            fishingrod: 0,
+            fishingroddurability: 0,
+            lastadventure: 0,
+            lastfishing: 0,
+            lastberburu: 0,
+            lastdungeon: 0,
+            lastslot: 0,
+            lastduel: 0,
+            lastmining: 0,
+            lasthunt: 0,
+            lastweekly: 0,
+            lastmonthly: 0,
+            registered: false,
+            name: this.getName(m.sender),
+            regTime: -1,
+            autolevelup: false,
+            ah: [],
+            mission: {},
         }
 
         let chat = global.db.data.chats[m.chat]

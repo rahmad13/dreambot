5c59c1d3da9fc4094beb1e3b3755566d6578f1c0
rahmad13 <93415263+rahmad13@users.noreply.github.com>
2021-11-17 02:42:47 +0700

    h

--- plugins/rpg-war.js
+++ plugins/rpg-war.js
@@ -6,52 +6,52 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   if(nmu.length == 0) return m.reply("Tag orang yang mau di attack!")
   nmu = nmu[0]
 
-  let musuh = global.DATABASE.data.users[nmu]
-  let kingdom = global.DATABASE.data.users[m.sender]
+  let musuh = global.db.data.users[nmu]
+  let kingdom = global.db.data.users[m.sender]
   if (kingdom.created == false || kingdom.created == undefined) return m.reply('Kamu belum memiliki kerajaan!')
   if (musuh.created == false || musuh.created == undefined) return m.reply('Dia belum memiliki kerajaan!')
   /* Diri Sendiri */
-  let kerajaan = global.DATABASE._data.users[m.sender].kingdomName
-  let troops = global.DATABASE._data.users[m.sender].troops
-  let lvl = global.DATABASE._data.users[m.sender].lvl
-  let koin = global.DATABASE.data.users[m.sender].koin
-  let batu = global.DATABASE._data.users[m.sender].besi
-  let kayu = global.DATABASE._data.users[m.sender].kayu
-  let besi = global.DATABASE.data.users[m.sender].batu
-  let emas = global.DATABASE._data.users[m.sender].emas
-  let makanan = global.DATABASE._data.users[m.sender].makanan
-  let lw = global.DATABASE.data.users[m.sender].lastwar || "-"
-  let al = global.DATABASE.data.users[m.sender].aliance
+  let kerajaan = global.db.data.users[m.sender].kingdomName
+  let troops = global.db.data.users[m.sender].troops
+  let lvl = global.db.data.users[m.sender].lvl
+  let koin = global.db.data.users[m.sender].koin
+  let batu = global.db.data.users[m.sender].besi
+  let kayu = global.db.data.users[m.sender].kayu
+  let besi = global.db.data.users[m.sender].batu
+  let emas = global.db.data.users[m.sender].emas
+  let makanan = global.db.data.users[m.sender].makanan
+  let lw = global.db.data.users[m.sender].lastwar || "-"
+  let al = global.db.data.users[m.sender].aliance
   /* Musuh */
-  let kerajaanM = global.DATABASE._data.users[nmu].kingdomName
-  let troopsM = global.DATABASE._data.users[nmu].troops
-  let lvlM = global.DATABASE._data.users[nmu].lvl
-  let koinM = global.DATABASE.data.users[nmu].koin
-  let batuM = global.DATABASE._data.users[nmu].besi
-  let kayuM = global.DATABASE._data.users[nmu].kayu
-  let besiM = global.DATABASE.data.users[nmu].batu
-  let emasM = global.DATABASE._data.users[nmu].emas
-  let makananM = global.DATABASE._data.users[nmu].makanan
-  let alM = global.DATABASE.data.users[nmu].aliance
+  let kerajaanM = global.db.data.users[nmu].kingdomName
+  let troopsM = global.db.data.users[nmu].troops
+  let lvlM = global.db.data.users[nmu].lvl
+  let koinM = global.db.data.users[nmu].koin
+  let batuM = global.db.data.users[nmu].besi
+  let kayuM = global.db.data.users[nmu].kayu
+  let besiM = global.db.data.users[nmu].batu
+  let emasM = global.db.data.users[nmu].emas
+  let makananM = global.db.data.users[nmu].makanan
+  let alM = global.db.data.users[nmu].aliance
 
   if(troops <= 0) return m.reply("Prajurit kamu tidak boleh 0 atau dibawah 0!")
 
   if(al.includes(nmu.split("@")[0])) return m.reply("Tidak dapat menyerang tim aliansi!")
 
   if(troops > troopsM) {
-    global.DATABASE.data.users[m.sender].lastwar = new Date * 1
-    global.DATABASE._data.users[nmu].troops -= troops
-    global.DATABASE._data.users[m.sender].troops += troopsM
-    global.DATABASE._data.users[m.sender].koin += (koinM - 30)
-    global.DATABASE._data.users[m.sender].batu += (batuM - 30)
-    global.DATABASE._data.users[m.sender].kayu += (kayuM - 30)
-    global.DATABASE._data.users[m.sender].besi += (besiM - 30)
-    global.DATABASE._data.users[m.sender].emas += (emasM - 30)
-    global.DATABASE._data.users[nmu].koin -= (koinM - 30)
-    global.DATABASE._data.users[nmu].batu -= (batuM - 30)
-    global.DATABASE._data.users[nmu].kayu -= (kayuM - 30)
-    global.DATABASE._data.users[nmu].besi -= (besiM - 30)
-    global.DATABASE._data.users[nmu].emas -= (emasM - 30)
+    global.db.data.users[m.sender].lastwar = new Date * 1
+    global.db.data.users[nmu].troops -= troops
+    global.db.data.users[m.sender].troops += troopsM
+    global.db.data.users[m.sender].koin += (koinM - 30)
+    global.db.data.users[m.sender].batu += (batuM - 30)
+    global.db.data.users[m.sender].kayu += (kayuM - 30)
+    global.db.data.users[m.sender].besi += (besiM - 30)
+    global.db.data.users[m.sender].emas += (emasM - 30)
+    global.db.data.users[nmu].koin -= (koinM - 30)
+    global.db.data.users[nmu].batu -= (batuM - 30)
+    global.db.data.users[nmu].kayu -= (kayuM - 30)
+    global.db.data.users[nmu].besi -= (besiM - 30)
+    global.db.data.users[nmu].emas -= (emasM - 30)
 
     m.reply(`Kamu memenangkan pertarungan!
 Kamu mendapatkan:
@@ -62,19 +62,19 @@ Kamu mendapatkan:
 ⛓️  ${besi + (besiM - 30)}
 🪙 ${emas + (emasM - 30)}`)
   } else if(troops < troopsM) {
-    global.DATABASE.data.users[m.sender].lastwar = new Date * 1
-    global.DATABASE._data.users[nmu].troops -= troops
-    global.DATABASE._data.users[m.sender].troops = 0
-    global.DATABASE._data.users[m.sender].koin -= (koinM - 30)
-    global.DATABASE._data.users[m.sender].batu -= (batuM - 30)
-    global.DATABASE._data.users[m.sender].kayu -= (kayuM - 30)
-    global.DATABASE._data.users[m.sender].besi -= (besiM - 30)
-    global.DATABASE._data.users[m.sender].emas -= (emasM - 30)
-    global.DATABASE._data.users[nmu].koin += (koinM - 30)
-    global.DATABASE._data.users[nmu].batu += (batuM - 30)
-    global.DATABASE._data.users[nmu].kayu += (kayuM - 30)
-    global.DATABASE._data.users[nmu].besi += (besiM - 30)
-    global.DATABASE._data.users[nmu].emas += (emasM - 30)
+    global.db.data.users[m.sender].lastwar = new Date * 1
+    global.db.data.users[nmu].troops -= troops
+    global.db.data.users[m.sender].troops = 0
+    global.db.data.users[m.sender].koin -= (koinM - 30)
+    global.db.data.users[m.sender].batu -= (batuM - 30)
+    global.db.data.users[m.sender].kayu -= (kayuM - 30)
+    global.db.data.users[m.sender].besi -= (besiM - 30)
+    global.db.data.users[m.sender].emas -= (emasM - 30)
+    global.db.data.users[nmu].koin += (koinM - 30)
+    global.db.data.users[nmu].batu += (batuM - 30)
+    global.db.data.users[nmu].kayu += (kayuM - 30)
+    global.db.data.users[nmu].besi += (besiM - 30)
+    global.db.data.users[nmu].emas += (emasM - 30)
     m.reply(`Kamu kalah!
 Kamu kehilangan:
 💰 ${koin + (koinM - 30)}

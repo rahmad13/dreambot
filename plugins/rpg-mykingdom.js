26caa95b55db5c0349590a3196a1bd20f659e5b0
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-11-24 14:40:21 +0700

    Update rpg-mykingdom.js

--- plugins/rpg-mykingdom.js
+++ plugins/rpg-mykingdom.js
@@ -48,7 +48,7 @@ _[ ❕ ]YOUR KINGDOM INFO_
 ${ benteng == 0 ? 'Tidak punya benteng' : '' || benteng == 1 ? 'benteng kayu' : '' || benteng == 2 ? 'benteng beton' : '' }
 ⚔️Last war:-
 🏳️teman Aliansi:
-${aliance}
+${aliance.replace(/@.+/, '')}
 ⚔️To war typing:
 ${usedPrefix}war @mention
 🏰Bangun Dan level up kerajaan

afcc257dadfa0d627fd7a34b9cf6e58f0cca3d96
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-11-30 17:12:11 +0700

    Update game-tekateki_hint.js

--- plugins/game-tekateki_hint.js
+++ plugins/game-tekateki_hint.js
@@ -1,12 +1,11 @@
+
 let handler = async (m, { conn }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
     if (!(id in conn.tekateki)) throw false
     let json = conn.tekateki[id][1]
-    let ans = json.jawaban.trim()
-    let clue = ans.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tekateki[id][0])
+    m.reply('```' + json.jawaban.toLowerCase().replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
 }
-handler.command = /^tete$/i
+handler.command = /^hah$/i
 handler.limit = true
 module.exports = handler

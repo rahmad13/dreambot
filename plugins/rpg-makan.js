e5a7f87110ed46ae00bf3084dbf49abdf7487a87
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-01 14:08:38 +0700

    Update rpg-makan.js

--- plugins/rpg-makan.js
+++ plugins/rpg-makan.js
@@ -2,13 +2,20 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
     let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
     let msgpenuh = (pickRandom(['staminamu sudah penuh', 'coba deh liat inv mu, staminamu kan dah 100 ngapai makan lagi?', 'stamina mu dah penuh woyy', 'ws kebek weh :v', 'staminamu dah penuh :v', 'udh weh, udh penuh']))
+    const Kchat = `
+${usedPrefix}makan <item> <jumlah>\n
+Contoh penggunaan: *${usedPrefix}makan ayamg 1*\n\n
+⚠️List Makanan:\n\n
+ayamg
+ayamb
+leleb
 let stamina = global.db.data.users[m.sender].stamina
  let kucing = global.db.data.users[m.sender].kucing
         let spertamina= (kucing == 0 ? 40 : '' || kucing == 1 ? 45 : '' || kucing == 2 ? 50 : '' || kucing == 3 ? 55 : '' || kucing == 4 ? 60 : '' || kucing == 5 ? 65 : '' || kucing == 6 ? 70 : '' || kucing == 7 ? 75 : '' || kucing == 8 ? 80 : '' || kucing == 9 ? 85 : '' || kucing == 10 ? 90 : '')
 try {
         if (/makan|eat/i.test(command)) {
         const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
-        if (args[0] === 'ayamg') {
+        case 'ayamg' :
         if (global.db.data.users[m.sender].stamina < 100) {
         	if (global.db.data.users[m.sender].ayamg >= count * 1) {
                             global.db.data.users[m.sender].ayamg -= count * 1
@@ -16,8 +23,8 @@ try {
                             conn.reply(m.chat, `Nyam nyam`, m)
                             } else conn.reply(m.chat, `Anda tidak memiliki ayam goreng` ,m)
         	} else conn.reply(m.chat, msgpenuh, m)
-        } else  if (args.length > 2 && args[0] === !'ayamg') m.reply('cuman bisa gunain ayam goreng')
-        if (args[0] === 'ayamb') {
+        break
+        case 'ayamb' :
         if (global.db.data.users[m.sender].stamina < 100) {
         	if (global.db.data.users[m.sender].ayamb >= count * 1) {
                             global.db.data.users[m.sender].ayamb -= count * 1
@@ -25,8 +32,8 @@ try {
                             conn.reply(m.chat, `Nyam nyam`, m)
                             } else conn.reply(m.chat, `Anda tidak memiliki ayam bakar` ,m)
         } else conn.reply(m.chat, msgpenuh, m)
-        } else  if (args.length > 2 && args[0] === !'ayamb') m.reply('cuman bisa gunain ayamgoreng')
-        if (args[0] === 'leleb') {
+        break
+        case 'leleb'
         if (global.db.data.users[m.sender].stamina < 100) {
         	if (global.db.data.users[m.sender].leleb >= count * 1) {
                             global.db.data.users[m.sender].leleb -= count * 1
@@ -34,7 +41,9 @@ try {
                             conn.reply(m.chat, `Nyam nyam`, m)
                             } else conn.reply(m.chat, `Anda tidak memiliki lele bakar` ,m)
         	} else conn.reply(m.chat, msgpenuh, m)
-        } else  if (args.length > 2 && args[0] === !'leleb') m.reply('cuman bisa gunain ayam bakar')
+        break
+        default:
+                    return conn.reply(m.chat, Kchat, m)
         }
      } catch (e) {
         console.log(e)
@@ -48,7 +57,7 @@ try {
     }
   }
   
-handler.help = ['makan <item> <jumlah>', 'eat']
+handler.help = ['makan <item> <jumlah>', 'heal']
 handler.tags = ['rpg']
 handler.command = /^(makan|eat)$/i
 handler.register = false

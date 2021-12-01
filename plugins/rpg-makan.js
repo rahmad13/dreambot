9a3742be2f7a905a4aff7f02afb357278174b84e
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-01 14:28:49 +0700

    Update rpg-makan.js

--- plugins/rpg-makan.js
+++ plugins/rpg-makan.js
@@ -1,6 +1,6 @@
 let { MessageType } = require('@adiwajshing/baileys')
-let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
-    let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
+let handler  = async (m, { conn, command, args, usedPrefix }) => {
+        let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
     let msgpenuh = (pickRandom(['staminamu sudah penuh', 'coba deh liat inv mu, staminamu kan dah 100 ngapai makan lagi?', 'stamina mu dah penuh woyy', 'ws kebek weh :v', 'staminamu dah penuh :v', 'udh weh, udh penuh']))
     const Kchat = `
 ${usedPrefix}makan <item> <jumlah>\n
@@ -13,17 +13,17 @@ leleb
 let stamina = global.db.data.users[m.sender].stamina
  let kucing = global.db.data.users[m.sender].kucing
         let spertamina= (kucing == 0 ? 40 : '' || kucing == 1 ? 45 : '' || kucing == 2 ? 50 : '' || kucing == 3 ? 55 : '' || kucing == 4 ? 60 : '' || kucing == 5 ? 65 : '' || kucing == 6 ? 70 : '' || kucing == 7 ? 75 : '' || kucing == 8 ? 80 : '' || kucing == 9 ? 85 : '' || kucing == 10 ? 90 : '')
-try {
+    try {
         if (/makan|eat/i.test(command)) {
-        const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
-        case 'ayamg' :
+            const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+                    case 'ayamg' :
         if (global.db.data.users[m.sender].stamina < 100) {
         	if (global.db.data.users[m.sender].ayamg >= count * 1) {
-                            global.db.data.users[m.sender].ayamg -= count * 1
-                            global.db.data.users[m.sender].stamina += spertamina * count
-                            conn.reply(m.chat, `Nyam nyam`, m)
-                            } else conn.reply(m.chat, `Anda tidak memiliki ayam goreng` ,m)
-        	} else conn.reply(m.chat, msgpenuh, m)
+                 global.db.data.users[m.sender].ayamg -= count * 1
+                 global.db.data.users[m.sender].stamina += spertamina * count
+          conn.reply(m.chat, `Nyam nyam`, m)
+           } else conn.reply(m.chat, `Anda tidak memiliki ayam goreng` ,m)
+           } else conn.reply(m.chat, msgpenuh, m)
         break
         case 'ayamb' :
         if (global.db.data.users[m.sender].stamina < 100) {
@@ -43,25 +43,24 @@ try {
                             } else conn.reply(m.chat, `Anda tidak memiliki lele bakar` ,m)
         	} else conn.reply(m.chat, msgpenuh, m)
         break
-        default:
+                default:
                     return conn.reply(m.chat, Kchat, m)
+            }
         }
-     } catch (e) {
+    } catch (e) {
+        conn.reply(m.chat, Kchat, m)
         console.log(e)
-        conn.reply(m.chat, msgerror, m)
         if (DevMode) {
-            let file = require.resolve(__filename)
             for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                conn.sendMessage(jid, file + ' error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
+                conn.sendMessage(jid, 'shop.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
             }
         }
     }
-  }
-  
-handler.help = ['makan <item> <jumlah>', 'heal']
+}
+
+handler.help = ['makan <args>', 'eat  <args>']
 handler.tags = ['rpg']
 handler.command = /^(makan|eat)$/i
-handler.register = false
 
 module.exports = handler
 

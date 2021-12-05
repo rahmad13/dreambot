b5a425df0c77fa1af407972b81bb1ce3fbfa59f0
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-05 21:38:23 +0700

    Update tebakkimia_hint.js

--- plugins/tebakkimia_hint.js
+++ plugins/tebakkimia_hint.js
@@ -3,8 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakkimia)) throw false
     let json = conn.tebakkimia[id][1]
-    let clue = json.unsur.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebakkimia[id][0])
+    m.reply('```' + json.unsur.replace(/[bcdfghjklmnpqrstvwxyz]/gi, '_') + '```')
 }
 handler.command = /^teki$/i
 handler.limit = true

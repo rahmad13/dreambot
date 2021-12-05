0307ddab1914a003bf8f37a74389410255f216a2
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-05 21:42:52 +0700

    Update tebakkimia.js

--- plugins/tebakkimia.js
+++ plugins/tebakkimia.js
@@ -1,23 +1,35 @@
-const similarity = require('similarity')
-const threshold = 0.72
-module.exports = {
-    async before(m) {
-        let id = m.chat
-        if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teki/i.test(m.quoted.text)) return !0
-        this.tebakkimia = this.tebakkimia ? this.tebakkimia : {}
-        if (!(id in this.tebakkimia)) return m.reply('Soal itu telah berakhir')
-        if (m.quoted.id == this.tebakkimia[id][0].id) {
-            let json = JSON.parse(JSON.stringify(this.tebakkimia[id][1]))
-            // m.reply(JSON.stringify(json, null, '\t'))
-            if (m.text.toLowerCase() == json.unsur.toLowerCase().trim()) {
-                global.db.data.users[m.sender].exp += this.tebakkimia[id][2]
-                m.reply(`*Benar!*\n+${this.tebakkimia[id][2]} XP`)
-                clearTimeout(this.tebakkimia[id][3])
-                delete this.tebakkimia[id]
-            } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-            else m.reply(`*Salah!*`)
-        }
-        return !0
-    },
-    exp: 0
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let src
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
+    let id = m.chat
+    if (id in conn.tebakkimia) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkimia[id][0])
+        throw false
+    }
+    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tebakkimia.json'))).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    if (!json) throw json
+    let caption = `
+Nama unsur dari lambang ${json.lambang} adalah...
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}teki untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.tebakkimia[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakkimia[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.unsur}*`, conn.tebakkimia[id][0])
+            delete conn.tebakkimia[id]
+        }, timeout)
+    ]
 }
+handler.help = ['tebakkimia']
+handler.tags = ['game']
+handler.command = /^tebakkimia/i
+
+module.exports = handler

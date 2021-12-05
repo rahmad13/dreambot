b8722b054809edc97d1a9426131c35972c75bde2
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-06 06:28:08 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,15 +1,42 @@
-let handler = async (m, { conn, text }) => {
-  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
+
+let handler  = async (m, { conn, text }) => {
+  let fs = require('fs')
+  let fetch = require('node-fetch')
+  const {
+    MessageType,
+    Mimetype
+} = require("@adiwajshing/baileys");
+const anu = {
+	"key": {
+		"fromMe": false,
+		"participant": "0@s.whatsapp.net",
+		"remoteJid": "0@s.whatsapp.net"
+	},
+	"message": {
+		"groupInviteMessage": {
+			"groupJid": "6285240750713-1610340626@g.us",
+			"inviteCode": "Dream∆Bot",
+			"groupName": "P", 
+            "caption": "「 All Group Broadcast 」", 
+            'jpegThumbnail': global.thumb
+		}
+	}
+}
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  try {
+    pp = await conn.getProfilePicture(who)}
+    catch (e){
+    }
+
+  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`, m)
-  for (let id of groups) {
-    await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Dream Broadcast 〕\n\n' + teks + '\n\n' + '® DreamBot'), true).catch(_ => _)
-  }
-  m.reply('_*Broadcast Selesai*_')
+  let content = await conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Group Broadcast 」')
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) conn.copyNForward(id, content, 'conversation',{quoted: anu, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: `© ${conn.user.name} || by Relldev` , body: '>///<', sourceUrl: 'https://chat.whatsapp.com/Jobz56qjFTyHVYuzQPnrih', thumbnail: global.thumb}}} ,true)
+  conn.reply(m.chat, `_Done_`, m)
 }
-handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
+handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 handler.owner = true
@@ -29,5 +56,3 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
-
-const delay = time => new Promise(res => setTimeout(res, time))

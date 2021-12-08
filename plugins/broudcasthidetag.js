f58991d1f23a04ee9b821a17aa9cf6aa26fd51d9
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-09 03:49:54 +0700

    Update broudcasthidetag.js

--- plugins/broudcasthidetag.js
+++ plugins/broudcasthidetag.js
@@ -1,10 +1,11 @@
 let handler  = async (m, { conn, text, participants }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
+  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
   let users = participants.map(u => u.jid)
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '✧༺ Dream∆Bot Broadcast ༻✧ '), true,  { contextInfo: { mentionedJid: users } }).catch(_=>_)
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '✧༺ Dream∆Bot Broadcast ༻✧ '), true,  { contextInfo: { mentionedJid: users } }).catch(_=>_)
   m.reply('Selesai Broadcast All Chat :)')
 }
 handler.help = ['bchidetag'].map(v => v + ' <teks>')

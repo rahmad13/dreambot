bbbc362cf4fb0f8182f47592905f6151ee935e7c
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-09 06:13:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
-  'https://melcanz.com': 'trial',
+  'https://melcanz.com': 'amel',
   'https://server-api-rey.herokuapp.com': 'apirey',
   'https://bx-hunter.herokuapp.com': 'Ikyy69',
   'https://hardianto-chan.herokuapp.com': 'hardianto',

09d69d17a72eff2a5bd63faca49ee33cbe050496
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-09 06:12:59 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '® Dream∆Bot', m)
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '® Dream∆Bot')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']

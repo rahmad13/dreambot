37bf1dac35e622e7609f674573945225648db268
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-12 10:18:49 +0700

    Update broudcastloc.js

--- plugins/broudcastloc.js
+++ plugins/broudcastloc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
     m.reply(`_mengirim pesan siaran ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
     for (let id of groups) {
-        await conn.delay(1500)
+        await delay(1500)
         await conn.sendButtonLoc(id, fla + 'broadcast', text, wm, 'broadcast', 'broadcast')
     }
     m.reply('*selesai*')

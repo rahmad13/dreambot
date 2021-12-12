aa61f115be1af03e0da546a4f73de0251892e82d
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-12 10:22:36 +0700

    Update d-instagram.js

--- plugins/d-instagram.js
+++ plugins/d-instagram.js
@@ -1,27 +1,3 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
-    for (let { downloadUrl, type } of json) {
-      await delay(1500)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*© Aine*', m, { thumbnail: Buffer.alloc(0) })
-    }
-  })
-}
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = true
-handler.premium = true
-
-module.exports = handler
-
-const delay = time => new Promise(res => setTimeout(res, time))*/
-
-
-
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const axios = require("axios")

e437a002ad12b827a0e77a9714f843b4170b1f44
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-15 03:24:16 +0700

    Create main-sewabot.js

---
+++ plugins/main-sewabot.js
@@ -0,0 +1,45 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+    pepe = 'https://telegra.ph/file/730f48f2524271f9c752a.jpg'
+    baper = await fetch(pepe).then(a => a.buffer())
+    let listMessage = {
+        "title": "melcanz-bot",
+        "description": `
+*melcanz-bot*
+        
+1 Grup / 30 Hari
+Rp. 5,000 Dana
+Rp. 10,000 Pulsa Telkomsel
+1 Grup / 60 Hari
+Rp. 10,000 Dana
+Rp. 15,000 Pulsa Telkomsel
+tertarik? hubungi:
+@6283108164709
+`.trim(),
+        "listType": "PRODUCT_LIST",
+        "productListInfo": {
+            "productSections": [
+                {
+                    "title": "Klik untuk melihat harga",
+                    "products": [
+                        {
+                            "productId": "4696956640315324"
+                        }
+                    ]
+                }
+            ],
+            "headerImage": {
+                "productId": "4696956640315324",
+                "jpegThumbnail": baper
+            },
+            "businessOwnerJid": "6283108164709@s.whatsapp.net"
+        },
+        "footerText": "wa.me/6283108164709"
+    }
+    conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
+}
+handler.help = ['sewabot']
+handler.tags = ['main']
+handler.command = /^sewa(bot|rhmd)$/i
+
+module.exports = handler
